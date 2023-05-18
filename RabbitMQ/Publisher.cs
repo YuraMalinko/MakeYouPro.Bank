130fe4d6d424505daa4bd85b3bbe017e83f5bac4
GothicXSPB <ZZZerggg@mail.ru>
2023-05-18 13:21:17 +0300

    add Publisher RabbitMQ

---
+++ RabbitMQ/Publisher.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using System.Text.Json;
+using RabbitMQ.Client;
+
+namespace RabbitMQ
+{
+    public class Publisher<T>
+    {
+        public T Value { get; set; }
+        private ConnectionFactory factory;
+        private IConnection connection;
+        private IModel channel;
+
+        public void SendMessege()
+        {
+            factory = new ConnectionFactory() { HostName = "localhost" };
+            connection = factory.CreateConnection();
+            channel = connection.CreateModel();
+
+            string queueName = "test";
+            var message = JsonSerializer.Serialize(Value);
+            var body = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish(exchange: "",
+                                routingKey: queueName,
+                                basicProperties: null,
+                                body: body);
+            Console.WriteLine($" [x] Sent {message}");
+
+            channel.Close();
+            connection.Close();
+        }
+    }
+}
