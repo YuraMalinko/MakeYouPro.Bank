d9baec7b7c79f4b37664a87e04ad67123f154b46
vladislavstr <58762058+vladislavstr@users.noreply.github.com>
2023-06-19 14:02:12 +0300

    add worker service

--- MakeYouPro.Bourse.LeadStatusUpdater.Service/Program.cs
+++ MakeYouPro.Bourse.LeadStatusUpdater.Service/Program.cs
@@ -1,25 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.ServiceProcess;
-using System.Text;
-using System.Threading.Tasks;
+using MakeYouPro.Bourse.LeadStatusUpdater.Service;
 
-namespace MakeYouPro.Bourse.LeadStatusUpdater.Service
-{
-    internal static class Program
+IHost host = Host.CreateDefaultBuilder(args)
+    .UseWindowsService()
+    .ConfigureServices(services =>
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        static void Main()
-        {
-            ServiceBase[] ServicesToRun;
-            ServicesToRun = new ServiceBase[]
-            {
-                new Service1()
-            };
-            ServiceBase.Run(ServicesToRun);
-        }
-    }
-}
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
---
+++ MakeYouPro.Bourse.LeadStatusUpdater.Service/Worker.cs
@@ -0,0 +1,33 @@
+namespace MakeYouPro.Bourse.LeadStatusUpdater.Service
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+
+        public Worker(ILogger<Worker> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation($"{nameof(Worker)}: Start");
+            return base.StartAsync(cancellationToken);
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation($"{nameof(Worker)} stopping.");
+            return base.StopAsync(cancellationToken);
+        }
+    }
+}
