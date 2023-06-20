63cfa9dca9e2611a5adec6587b3053b1c1288e3d
vladislavstr <58762058+vladislavstr@users.noreply.github.com>
2023-06-20 23:40:28 +0300

    add launch time

--- MakeYouPro.Bourse.LeadStatusUpdater.Service/Worker.cs
+++ MakeYouPro.Bourse.LeadStatusUpdater.Service/Worker.cs
@@ -4,17 +4,48 @@ namespace MakeYouPro.Bourse.LeadStatusUpdater.Service
     {
         private readonly ILogger<Worker> _logger;
 
+        private readonly HttpClient _httpClient;
+
         public Worker(ILogger<Worker> logger)
         {
             _logger = logger;
+            _httpClient = new HttpClient();
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
+                DateTime currentTime = DateTime.Now;
+
+                if (currentTime.Hour == 22 && currentTime.Minute == 00)
+                {
+                    await ProcessDataAsync();
+                }
+
+                await Task.Delay(TimeSpan.FromMinutes(1), stoppingToken);
+            }
+        }
+
+        private async Task ProcessDataAsync()
+        {
+            try
+            {
+                HttpResponseMessage response = await _httpClient.GetAsync("https://api.example.com/data");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string data = await response.Content.ReadAsStringAsync();
+
+                }
+                else
+                {
+                    Console.WriteLine($"Ошибка при выполнении GET-запроса: {response.StatusCode}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка во время выполнения GET-запроса: {ex.Message}");
             }
         }
 
