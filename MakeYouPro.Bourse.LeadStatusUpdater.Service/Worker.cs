e8798fdd723966c6109ade1df4abdf4a557a485c
vladislavstr <58762058+vladislavstr@users.noreply.github.com>
2023-06-28 20:46:58 +0300

    add GetSettings

--- MakeYouPro.Bourse.LeadStatusUpdater.Service/Worker.cs
+++ MakeYouPro.Bourse.LeadStatusUpdater.Service/Worker.cs
@@ -1,14 +1,17 @@
+using System.Text.Json;
+
 namespace MakeYouPro.Bourse.LeadStatusUpdater.Service
 {
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
-
+        private readonly IConfiguration _configuration;
         private readonly HttpClient _httpClient;
 
-        public Worker(ILogger<Worker> logger)
+        public Worker(ILogger<Worker> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
             _httpClient = new HttpClient();
         }
 
@@ -18,8 +21,11 @@ namespace MakeYouPro.Bourse.LeadStatusUpdater.Service
             {
                 DateTime currentTime = DateTime.Now;
 
-                if (currentTime.Hour == 22 && currentTime.Minute == 00)
+                if (currentTime.Hour == 20)
                 {
+                    var settings = new Settings();
+                    settings = GetSettings();
+                    Console.WriteLine(settings.PeriodOfTransactionsInDays);
                     await ProcessDataAsync();
                 }
 
@@ -31,21 +37,39 @@ namespace MakeYouPro.Bourse.LeadStatusUpdater.Service
         {
             try
             {
-                HttpResponseMessage response = await _httpClient.GetAsync("https://api.example.com/data");
+                using (HttpClient client = _httpClient)
+                {
+                    HttpResponseMessage response = await _httpClient.GetAsync("https://api.example.com/data");
 
-                if (response.IsSuccessStatusCode)
-                {
-                    string data = await response.Content.ReadAsStringAsync();
-                }
-                else
-                {
-                    Console.WriteLine($"Ошибка при выполнении GET-запроса: {response.StatusCode}");
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string data = await response.Content.ReadAsStringAsync();
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Ошибка при выполнении GET-запроса: {response.StatusCode}");
+                    }
+
                 }
             }
             catch (Exception ex)
             {
                 Console.WriteLine($"Ошибка во время выполнения GET-запроса: {ex.Message}");
             }
+        }
+
+        private Settings GetSettings()
+        {
+            string _path = _configuration.GetSection("AppSettings:PathToSettings").Value!;
+
+            using (StreamReader StreamWriter = new StreamReader(_path))
+            {
+                var result = new Settings();
+                string jsn = StreamWriter.ReadLine();
+                result = JsonSerializer.Deserialize<Settings>(jsn);
+                return result;
+            }
+
         }
 
         public override Task StartAsync(CancellationToken cancellationToken)
