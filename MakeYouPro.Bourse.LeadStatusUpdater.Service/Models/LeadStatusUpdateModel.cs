08befa64fde34688f934c99c50a5eea36c494a7d
andreymuskat <andrey.strel.2001@gmail.com>
2023-06-29 00:00:59 +0300

    add model LeadStatusUpdateModel

---
+++ MakeYouPro.Bourse.LeadStatusUpdater.Service/Models/LeadStatusUpdateModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MakeYouPro.Bourse.LeadStatusUpdater.Service.Models
+{
+    public class LeadStatusUpdateModel
+    {
+        public int Id { get; set; }
+
+        public int Status { get; set; }
+    }
+}
