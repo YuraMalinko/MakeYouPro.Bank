eeb1278e654890f834029d5fd976432f8eafff7a
DariaKostrik <dolorez0927@yandex.ru>
2023-06-29 14:07:40 +0300

    small changes

--- WebAPIClient/Program.cs
+++ WebAPIClient/Program.cs
@@ -17,24 +17,6 @@ public class Program
         IHost host = builder.Build();
         host.Run();
 
-        var factory = new ConnectionFactory { HostName = "main" };
-        using var connection = factory.CreateConnection();
-        using var channel = connection.CreateModel();
-
-
-        channel.QueueDeclare(queue: "MakeYouPro",
-                     durable: false,
-                     exclusive: false,
-                     autoDelete: false,
-                     arguments: null);
-        string mail = "test";
-        var body = Encoding.UTF8.GetBytes(mail);
-
-        channel.BasicPublish(exchange: string.Empty,
-                             routingKey: "ratesProvider",
-                             basicProperties: null,
-                             body: body);
-        Console.WriteLine($" [x] Sent ");
 
         Console.ReadLine();
     }
